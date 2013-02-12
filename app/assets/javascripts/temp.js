6026d832d7e372cc369461d4a8dd40858e03fdab
Alec Embke <aembke@uoregon.edu>
2013-02-11 20:25:14 -0800

    oh lovely, my audio drivers dont work in linux anymore. saving and switching to windows...

---
+++ app/assets/javascripts/temp.js
@@ -0,0 +1,44 @@
+/*
+ * https://developer.mozilla.org/en-US/docs/WebRTC/Taking_webcam_photos
+ * 
+ */
+
+
+(function($){
+	
+	$(document).ready(function(){
+		var app = new App();
+		app.init();
+	});
+	
+	
+	var App = function(){
+		var settings = {
+			
+			
+			
+			
+		},
+		that = {
+			
+			
+			
+			init: function(){
+				
+			}
+			
+		};
+		
+		
+		
+		
+		
+		
+		
+		
+	};
+	
+	
+	
+	
+}(jQuery));
